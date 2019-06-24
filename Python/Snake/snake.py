29cdcf8020f8ef3284a0d4a85c19f1773098e5d2
LuRuz <luciaruzsaez@gmail.com>
2019-06-24 18:12:35 +0200

    snake funciona

--- Python/Snake/snake.py
+++ Python/Snake/snake.py
@@ -19,6 +19,7 @@ red= pygame.image.load("imagenSnake/red.png")
 
 posX, posY= 15,15 #randint(0,TamanioX), randint(0, TamanioY)
 posXR, posYR=15,15
+SNAKE= [(posX, posY)]
 
 
 velSnake= 50
@@ -29,7 +30,7 @@ tiempo= 0
 
 #Funcion que crea posicion aleatoria para la casilla roja
 def positionRed():
-    print ("HE ENTRADO A CREAR LA POSICION")
+    #print ("HE ENTRADO A CREAR LA POSICION")
     posXR, posYR= randint(0,TamanioX-100), randint(0, TamanioY-100)
     centrado= False
 
@@ -46,30 +47,55 @@ def positionRed():
 
 #loop para el juego
 while True:
-    print (tiempo, level)
+    #print (tiempo, level)
+
 
     #Dependiendo del nivel va a una velocidad u a otra.
     if tiempo> level:
         if lastAction=='LEFT':
             posX-= velSnake
+            if (posX, posY) not in SNAKE:
+                SNAKE.append((posX,posY))
+                del SNAKE [0:1]
+            else:
+                pygame.quit()
+                sys.exit()
             if posX<0:
                 pygame.quit()
                 sys.exit()
                 print ('GAME OVER')
         elif lastAction=='RIGHT':
             posX+= velSnake
+            if (posX, posY) not in SNAKE:
+                SNAKE.append((posX,posY))
+                del SNAKE [0:1]
+            else:
+                pygame.quit()
+                sys.exit()
             if posX>TamanioX:
                 pygame.quit()
                 sys.exit()
                 print ('GAME OVER')
         elif lastAction== 'UP':
             posY-= velSnake
+            if (posX, posY) not in SNAKE:
+                SNAKE.append((posX,posY))
+                del SNAKE [0:1]
+            else:
+                pygame.quit()
+                sys.exit()
             if posY<0:
                 pygame.quit()
                 sys.exit()
                 print ('GAME OVER')
         elif lastAction=='DOWN':
             posY +=velSnake
+            if (posX, posY) not in SNAKE:
+                SNAKE.append((posX,posY))
+                del SNAKE [0:1]
+            else:
+                pygame.quit()
+                sys.exit()
             if posY>TamanioY:
                 pygame.quit()
                 sys.exit()
@@ -105,42 +131,60 @@ while True:
                     posX-= velSnake
                     tiempo =0
                     lastAction= 'LEFT'
+                    if (posX, posY) not in SNAKE:
+                        SNAKE.append((posX,posY))
+                        del SNAKE [0:1]
+                    else:
+                        pygame.quit()
+                        sys.exit()
             elif evento.key == K_RIGHT:
                 if posX<(TamanioX-50):
                     posX+= velSnake
                     tiempo =0
                     lastAction= 'RIGHT'
+                    if (posX, posY) not in SNAKE:
+                        SNAKE.append((posX,posY))
+                        del SNAKE [0:1]
+                    else:
+                        pygame.quit()
+                        sys.exit()
+
             elif evento.key == K_UP:
                 if posY>15:
                     posY-= velSnake
                     tiempo =0
                     lastAction= 'UP'
+                    if (posX, posY) not in SNAKE:
+                        SNAKE.append((posX,posY))
+                        del SNAKE [0:1]
+                    else:
+                        pygame.quit()
+                        sys.exit()
             elif evento.key == K_DOWN:
                 if posY<(TamanioY-50):
                     posY+= velSnake
                     tiempo =0
                     lastAction= 'DOWN'
+                    if (posX, posY) not in SNAKE:
+                        SNAKE.append((posX,posY))
+                        del SNAKE [0:1]
+                    else:
+                        pygame.quit()
+                        sys.exit()
 
     #print ("Posicion negro:", posX, posY)
     #print ("Posicion rojo: ", posXR, posYR)
+
+    #Si el rojo y el negro se encuentran en el mismo punto se come
     if posX == posXR and posY== posYR:
         comido = True
-        #ventana.blit(black, (posXR, posYR))
+        SNAKE.append((posX,posY))
 
+    #Imprime toda la lista de la serpiente
+    for i in SNAKE:
+        ventana.blit(black, i)
+#        print ( i)
     tiempo +=1
-    '''
-#mueve el objeto hacia la derecha e izquierda hasta que llega al final del tablero
-    #print (posX, derecha, TamanioX)
-    if movement == True:
-        if posYCo>-100:
-            ventana.blit(cohete, (posXCo, posYCo))
-            posYCo-= velCohete
-        else:
-            movement=False
-
-
-    #uso del cursor
-    #posXC, posYC= pygame.mouse.get_pos()
-    '''
+    
     #Actualiza lo que esta pasando en la ventana
     pygame.display.update()
