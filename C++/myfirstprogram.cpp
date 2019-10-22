6b8dcb6acc662c0860a964d56d8b5b5a4809c54e
LuRuz <luciaruzsaez@gmail.com>
2019-10-22 21:05:55 +0200

    first c++

---
+++ C++/myfirstprogram.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string test;
+    cout << "Introduzca string: "; //Output text
+    //cin >> x; //Get user input from keyboard. ONLY FOR SIMPLE WORDS
+    getline (cin, test); //Imput line from keyboard
+    cout << "El string introducido: " << test;
+
+    cout << "\n";
+    int y=1;
+    y <<= 2;    //Mueve el bit 1, dos elementos hacia la izda
+    cout << y;
+    cout << "\n";
+
+    string z= "prueba \n";
+    cout << z.length(); //La longitud del string incluye solo los caracteres
+
+
+    return 0;
+}
